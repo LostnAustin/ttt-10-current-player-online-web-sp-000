86da9c2cb8c472bcbf1c411c0e14bc54ac3c6b0d
Chad Alan Lengefeld <chadlengefeld@gmail.com>
2019-10-16 02:26:57 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+def turn_count(board)
+
+ count = 0
+  board.each do |turn|
+    if (turn == "X" || turn == "O")
+   count += 1
+    end
+# board.each{|turn| puts "#{turn}"}
+  end
+  return count
+end
+
+
+
+def current_player(board)
+
+  if ((turn_count(board) % 2) == true)
+    return "O"
+else
+     ((turn_count(board) % 2) != true)
+    return "X"
+  end
+end
